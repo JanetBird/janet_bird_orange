0b77806446492999c7638f43185fcc4d62849e59
Anna Ionova <echoespond@gmail.com>
2018-11-24 13:08:27 -0500

    work on random word display, reseting the form & highlight toggle of rhyme selections

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
 const rhymesApp = {
-
-    firstLineWord: '',
-    secondLineWord: '',
-    thirdLineOptions: [],
-    fourthLineOptions: [],
-
+    
     firstLine: '',
     secondLine: '',
     thirdLine: '',
@@ -30,21 +25,51 @@ rhymesApp.displayRhymeOptions = function(options, containerID) {
     const container = $(`#${containerID}`);
     // looping through an array & adding elements to display rhyme options
     options.forEach(option => {
-        const optionElement = `<p>${option}</p>`;
+        // create word options in HTML
+        const optionElement = `<p class="rhymeOption">${option}</p>`;
+        // & display on the page
         container.append(optionElement);
     });
 }
 
-// function running on form submit
-rhymesApp.goToStepTwo = function() {
-    // gets user input (first two sentences) and saves it in a variable
-    const userInputFirstLine = $('#firstLine').val();
-    const userInputSecondLine = $('#secondLine').val();
+rhymesApp.getRandomRhymes = function (arr, originalWord, numElements) {
+    // numElements is how many random rhymes we want to get in the array
+    const randomRhymes = [];
 
+    // use 'for' loop instead of 'while' to not accidentally create infinite loops in edge cases 
+    for (let i = 0; i < arr.length; i++) {
+        if (randomRhymes.length === numElements) {
+            // break out of the loop as soon as we have collected the required number of elements
+            break;
+        }
+        // get random rhyme option from the original array
+        let randomArrIndex = Math.floor(Math.random() * arr.length - 1);
+        let currentRhyme = arr[randomArrIndex];
+
+
+        // as we checked what results API gives us, we need to filter out the original word if it is present in the results coming back from the API
+
+        // if the random rhyme is not the original word and if it's not already in the array of random words (not to have repeated rhyme options on display) add it to the list
+        if (currentRhyme !== originalWord && randomRhymes.indexOf(currentRhyme) === -1) {
+            randomRhymes.push(currentRhyme);
+        }
+    }
+
+    return randomRhymes;
+}
+
+// This function is run when user enters their poem's first 2 lines, and clicks the button to go to the next step
+rhymesApp.goToStepTwo = function() {
+
+    $('.rhymeOption').remove();
+
+    // gets user input (first two sentences) and saves it in a variable
+    const userInputFirstLine = $('#firstLineInput').val();
+    const userInputSecondLine = $('#secondLineInput').val();
+    
     // save lines values to display later
     rhymesApp.firstLine = userInputFirstLine;
     rhymesApp.secondLine = userInputSecondLine;
-
 
     // extracts & saves the last words from the user input strings 
     const lastWordFirstLine = userInputFirstLine.split(" ").splice(-1);
@@ -53,25 +78,26 @@ rhymesApp.goToStepTwo = function() {
     // saving both words in array
     const words = [lastWordFirstLine, lastWordSecondLine];
 
-    // calling rhymesApp.getRhyme function to 
+    // calling rhymesApp.getRhyme function to get response with rhymes for all elements of words array from the API
     const wordPromises = words.map(rhymesApp.getRhyme);
     // spreading wordPromises array
     $.when(...wordPromises)
         .then((...result) => {
-
             result.forEach(result => {
                 let response = result[0];
                 let chosenOptions = [];
-
+                console.log('responseeeee',response);
                 // THIRD LINE CONTENTS
                 if (response.word === lastWordFirstLine[0]) {
                     const thirdLineOptions = response.rhymes.all;
                 
-                    if (thirdLineOptions.length === 0) {
+                    if (thirdLineOptions === undefined) { 
+                        // will return orange on every "help me rhyme click", unless the page is refreshed
                         chosenOptions = ['Orange'];
-                    // choose random up to 20 words for third line
+                   
                     } else if (thirdLineOptions.length > 20) {
-                        chosenOptions = thirdLineOptions.slice(0, 20);
+
+                        chosenOptions = rhymesApp.getRandomRhymes(thirdLineOptions, lastWordFirstLine[0], 20);
                     } else {
                         chosenOptions = thirdLineOptions;
                     }
@@ -83,11 +109,12 @@ rhymesApp.goToStepTwo = function() {
                 if (response.word === lastWordSecondLine[0]) {
                     const fourthLineOptions = response.rhymes.all;
                     
-                    if (fourthLineOptions.length === 0) {
+                    if (fourthLineOptions === undefined) { 
                         chosenOptions = ['Orange'];
-                    // choose random up to 20 words for fourth line
+                        
                     } else if (fourthLineOptions.length > 20) {
-                        chosenOptions = fourthLineOptions.slice(0, 20);
+
+                        chosenOptions = rhymesApp.getRandomRhymes(fourthLineOptions, lastWordSecondLine[0], 20);
                     } else {
                         chosenOptions = fourthLineOptions;
                     }
@@ -95,27 +122,62 @@ rhymesApp.goToStepTwo = function() {
                     rhymesApp.displayRhymeOptions(chosenOptions, 'fourthLineOptions');
                 }
             });
+
+            // created all the options and can be added the highlight on click functionality to rhyme options
+            $('.rhymeOption').click(function() {
+                $(this).toggleClass('selectedRhymeOption');
+            });
         });
 
+    // Scroll the user to the Step 2 section smoothly
     $([document.documentElement, document.body]).animate({
         scrollTop: $(".stepTwo").offset().top
     }, 1500);
-
-
-    // create word options in HTML
-
-
-    // add word hightlight functionality using $(this).toggleClass('selectedRhymeOption') OR css active
 }
 
 rhymesApp.goToStepThree = function () {
-    console.log("goToStepThree runs");
+    const userInputThirdLine = $('#thirdLineInput').val();
+    const userInputFourthLine = $('#fourthLineInput').val();
 
+    rhymesApp.thirdLine = userInputThirdLine;
+    rhymesApp.fourthLine = userInputFourthLine;
 
+    const arrayOfSentences = [rhymesApp.firstLine, rhymesApp.secondLine, rhymesApp.thirdLine, rhymesApp.fourthLine];
+
+    // Scroll the user to the Step Three section smoothly
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".stepThree").offset().top
+    }, 1500);
+
+    // function that puts a 4 line poem on the page
+    const sentencesContainer = $('#poemDisplayContainer');
+    // looping through the array of poem lines
+    arrayOfSentences.forEach(sentence => {
+        // create sentences in HTML
+        const sentenceElement = `<div><p class="poemLine">${sentence}</p></div>`;
+        // & display on the page
+        sentencesContainer.append(sentenceElement);
+    });
 }
 
 rhymesApp.restart = function () {
+    rhymesApp.firstLine = '';
+    rhymesApp.secondLine = '';
+    rhymesApp.thirdLine = '';
+    rhymesApp.fourthLine = '';
 
+    // we need to clear inputs
+    $('#firstLine').val('');
+    $('#secondLine').val('');
+    $('#thirdLine').val('');
+    $('#fourthLine').val('');
+
+    //delete all craeted paragraphs(rhyme options)
+    // scroll up to the top of the page
+    // we can scroll to the step one section
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $(".stepOne").offset().top
+    }, 1500);
 }
 
 
@@ -150,7 +212,6 @@ function rotate() {
 }
 
 
-
 // init function to get the app going
 rhymesApp.init = function () {
     const url = location.href;
@@ -159,16 +220,7 @@ rhymesApp.init = function () {
         // only run this code on home page.
         setInterval(rotate, 1000);
     }
-    
     rhymesApp.setupUserEvents();
-    // rhymesApp.goToStepTwo();
-    // rhymesApp.goToStepThree();
-
-    // console.log('location');
-    // console.log(location.href);
-   
-
-
 };
 
 
